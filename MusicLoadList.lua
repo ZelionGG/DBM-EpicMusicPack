30c8f1b10975f13535f0e546e19e8121c8e73865
Aurélien ALVES <contact@zeliongg.com>
2024-06-07 01:13:21 +0200

    Fully reworked the Music Loader to make it easier to manually add songs.

--- MusicLoadList.lua
+++ MusicLoadList.lua
@@ -1,283 +1,66 @@
-local tinsert = table.insert
-local inserted = false
+local function insertMusic(musicName, musicId)
+    if DBM and DBM.Music and DBM.DungeonMusic and DBM.BattleMusic then
+        local musicPath = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\"
+        local musicEntry = {text = "EMP - " .. musicName, value = musicPath .. musicId .. ".mp3"}
+
+        table.insert(DBM.Music, musicEntry)
+        if DBM.DungeonMusic then table.insert(DBM.DungeonMusic, musicEntry) end
+        if DBM.BattleMusic then table.insert(DBM.BattleMusic, musicEntry) end
+    end
+end
+
+
+-- This table contains all the music files that DBM will use. The keys are the names
+-- of the files without the extension, and the values are the names of the files as
+-- they should appear in the music drop-down menus. The files are located in the
+-- DBM-EpicMusicPack\Music directory.
+
+-- Each line of the table follows the following format:
+
+-- ["file_name_without_extension"] = "display_name_as_it_should_appear_in_drop_down_menus"
+
+-- For example:
+
+-- ["file_name_without_extension"] = "Display Name as it should appear in drop-down menus"
+
+-- The files are inserted into the DBM.Music, DBM.DungeonMusic, and DBM.BattleMusic
+-- tables, if they exist.
+local musicTable = {
+    ["EMP_ES_JuggernautMarch"] = "FormantX - Juggernaut March",
+    ["EMP_ES_QuantumSonata"] = "FormantX - Quantum Sonata",
+    ["EMP_ES_OurLastStand"] = "Niklas Johansson - Out Last Stand",
+    ["EMP_ES_Vanguard"] = "Jo Blankenburg - Vanguard",
+    ["EMP_ES_LastFightforFreedom"] = "Edgar Hopp - Last Fight for Freedom",
+    ["EMP_ES_LastStrikeForGlory"] = "Dream Cave - Last Strike for Glory",
+    ["EMP_ES_TrailsOfLegends"] = "Bonnie Grace - Trails of Legends",
+    ["EMP_ES_KarmaBullet"] = "Robert Ruth - Karma Bullet",
+    ["EMP_ES_MyCondolencesMonster"] = "Sham Stalin & Pandora Journey - My Condolences, Monster",
+    ["EMP_ES_Abeyance"] = "Mark Petrie - Abeyance",
+    ["EMP_Oblivion"] = "Baptiste Thiry - Oblivion",
+    ["EMP_SoSayWeAll"] = "Audiomachine - So Say We All",
+    ["EMP_AbsoluteMagnitude"] = "Audiomachine - Absolute Magnitude",
+    ["EMP_MS_FreedomFighters"] = "Machinimasound - Freedom Fighters",
+    ["EMP_MS_Mercenaries"] = "Machinimasound - Mercenaries",
+    ["EMP_2WEI_Pandora"] = "2WEI - Pandora",
+    ["EMP_2WEI_Insomnia"] = "2WEI - Insomnia",
+    ["EMP_RSM_ThunderFist"] = "Really Slow Motion - Thunder Fist",
+    ["EMP_RSM_PointOfOrigin"] = "Really Slow Motion - Point of Origin",
+    ["EMP_CAP_BattleAgainstTime"] = "Celestial Aeon Project - Battle Against Time",
+    ["EMP_HSM_HoldTheBeacon"] = "Hypersonic Music - Hold the Beacon",
+    ["EMP_HSM_Tesseract"] = "Hypersonic Music - Tesseract",
+}
 
 function DBMMPEpicMusicPack()
-	if inserted then 
-        return 
-    end
-    -- All Music Table
-	tinsert(DBM.Music, {
-        text = "EMP - FormantX - Juggernaut March", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_JuggernautMarch.mp3"
-    })
-    tinsert(DBM.Music, {
-        text = "EMP - FormantX - Quantum Sonata", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_QuantumSonata.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Niklas Johansson - Out Last Stand", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_OurLastStand.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Jo Blankenburg - Vanguard", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_Vanguard.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Edgar Hopp - Last Fight for Freedom", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_LastFightforFreedom.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Dream Cave - Last Strike for Glory", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_LastStrikeForGlory.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Bonnie Grace - Trails of Legends", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_TrailsOfLegends.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Robert Ruth - Karma Bullet", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_KarmaBullet.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Sham Stalin & Pandora Journey - My Condolences, Monster", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_MyCondolencesMonster.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Mark Petrie - Abeyance", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_Abeyance.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Baptiste Thiry - Oblivion", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_Oblivion.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Audiomachine - So Say We All", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_SoSayWeAll.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Audiomachine - Absolute Magnitude", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_AbsoluteMagnitude.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Machinimasound - Freedom Fighters", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_MS_FreedomFighters.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Machinimasound - Mercenaries", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_MS_Mercenaries.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - 2WEI - Pandora", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_2WEI_Pandora.mp3"
-    })
-    tinsert(DBM.Music, {
-        text = "EMP - 2WEI - Insomnia", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_2WEI_Insomnia.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Really Slow Motion - Thunder Fist", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_RSM_ThunderFist.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Really Slow Motion - Point of Origin", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_RSM_PointOfOrigin.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Celestial Aeon Project - Battle Against Time", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_CAP_BattleAgainstTime.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Hypersonic Music - Hold the Beacon", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_HSM_HoldTheBeacon.mp3"
-    })
-	tinsert(DBM.Music, {
-        text = "EMP - Hypersonic Music - Tesseract", 
-        value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_HSM_Tesseract.mp3"
-    })
-
-    -- Dungeon BGM Table
-	if DBM.DungeonMusic then
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - FormantX - Juggernaut March", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_JuggernautMarch.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - FormantX - Quantum Sonata", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_QuantumSonata.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Niklas Johansson - Out Last Stand", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_OurLastStand.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Jo Blankenburg - Vanguard", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_Vanguard.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Edgar Hopp - Last Fight for Freedom", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_LastFightforFreedom.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Dream Cave - Last Strike for Glory", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_LastStrikeForGlory.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Bonnie Grace - Trails of Legends", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_TrailsOfLegends.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Robert Ruth - Karma Bullet", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_KarmaBullet.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Sham Stalin & Pandora Journey - My Condolences, Monster", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_MyCondolencesMonster.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Mark Petrie - Abeyance", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_Abeyance.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Baptiste Thiry - Oblivion", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_Oblivion.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Audiomachine - So Say We All", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_SoSayWeAll.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Audiomachine - Absolute Magnitude", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_AbsoluteMagnitude.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Machinimasound - Freedom Fighters", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_MS_FreedomFighters.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Machinimasound - Mercenaries", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_MS_Mercenaries.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - 2WEI - Pandora", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_2WEI_Pandora.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - 2WEI - Insomnia", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_2WEI_Insomnia.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Really Slow Motion - Thunder Fist", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_RSM_ThunderFist.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Really Slow Motion - Point of Origin", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_RSM_PointOfOrigin.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Celestial Aeon Project - Battle Against Time", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_CAP_BattleAgainstTime.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Hypersonic Music - Hold the Beacon", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_HSM_HoldTheBeacon.mp3"
-        })
-        tinsert(DBM.DungeonMusic, {
-            text = "EMP - Hypersonic Music - Tesseract", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_HSM_Tesseract.mp3"
-        })
-    end
-
-    -- Boss BGM Table
-	if DBM.BattleMusic then
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - FormantX - Juggernaut March", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_JuggernautMarch.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - FormantX - Quantum Sonata", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_QuantumSonata.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Niklas Johansson - Out Last Stand", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_OurLastStand.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Jo Blankenburg - Vanguard", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_Vanguard.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Edgar Hopp - Last Fight for Freedom", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_LastFightforFreedom.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Dream Cave - Last Strike for Glory", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_LastStrikeForGlory.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Bonnie Grace - Trails of Legends", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_TrailsOfLegends.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Robert Ruth - Karma Bullet", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_KarmaBullet.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Sham Stalin & Pandora Journey - My Condolences, Monster", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_MyCondolencesMonster.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Mark Petrie - Abeyance", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_ES_Abeyance.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Baptiste Thiry - Oblivion", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_Oblivion.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Audiomachine - So Say We All", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_SoSayWeAll.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Audiomachine - Absolute Magnitude", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_AbsoluteMagnitude.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Machinimasound - Freedom Fighters", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_MS_FreedomFighters.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Machinimasound - Mercenaries", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_MS_Mercenaries.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - 2WEI - Pandora", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_2WEI_Pandora.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - 2WEI - Insomnia", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_2WEI_Insomnia.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Really Slow Motion - Thunder Fist", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_RSM_ThunderFist.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Really Slow Motion - Point of Origin", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_RSM_PointOfOrigin.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Celestial Aeon Project - Battle Against Time", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_CAP_BattleAgainstTime.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Hypersonic Music - Hold the Beacon", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_HSM_HoldTheBeacon.mp3"
-        })
-        tinsert(DBM.BattleMusic, {
-            text = "EMP - Hypersonic Music - Tesseract", 
-            value = "Interface\\AddOns\\DBM-EpicMusicPack\\Music\\EMP_HSM_Tesseract.mp3"
-        })
+    local musicOrder = {}
+    for musicId, musicName in pairs(musicTable) do
+        table.insert(musicOrder, {id = musicId, name = musicName})
     end
     
-	inserted = true
+    table.sort(musicOrder, function(a, b)
+        return a.name < b.name
+    end)
+    
+    for _, music in ipairs(musicOrder) do
+        insertMusic(music.name, music.id)
+    end
 end
